be3ae4e766a078596b3cb955f9679370cb3aac5d
Bharat Agarwal <bharatagarwal@gmail.com>
2024-03-18 15:06:06 +0530

    Removing Payload logo from header

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -28,18 +28,7 @@ export async function Header() {
     <>
       <header className={classes.header}>
         <Gutter className={classes.wrap}>
-          <Link href="/">
-            {/* Cannot use the `<picture>` element here with `srcSet`
-              This is because the theme is able to be overridden by the user
-              And so `@media (prefers-color-scheme: dark)` will not work
-              Instead, we just use CSS to invert the color via `filter: invert(1)` based on `[data-theme="dark"]`
-            */}
-            <img
-              className={classes.logo}
-              alt="Payload Logo"
-              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
-            />
-          </Link>
+          {/* Removing logo from header */}
           <HeaderNav header={header} />
         </Gutter>
       </header>
